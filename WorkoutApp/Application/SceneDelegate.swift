35781d6167b96a399f443aa8d7251a982a717a54
Vik Kalnoy <kalnoy.v@yandex.ru>
2022-10-05 22:58:18 +0300

    make tabBar

---
+++ WorkoutApp/Application/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  WorkoutApp
+//
+//  Created by Вик Кальной on 04.10.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+
+        let tabBarController = TabBarController()
+        
+        guard let windowScene =  (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+    }
+
+}
+
